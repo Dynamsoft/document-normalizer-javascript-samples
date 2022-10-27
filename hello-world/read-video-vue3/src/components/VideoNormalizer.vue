446c82ccb78dcc688f0e11a587f9d5bfd1d2ea13
SHEN <1014628363@qq.com>
2022-10-27 16:56:35 +0800

    DDNJS 1.0.0

--- hello-world/read-video-vue3/src/components/VideoNormalizer.vue
+++ hello-world/read-video-vue3/src/components/VideoNormalizer.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="width: 100%; height: 100%">
     <div class="video-normalizer-btns">
-      <button class="editor-border-btn" @click="confirmQuadForNormalization">confirmQuadForNormalization</button>
-      <button class="end-editor-btn" @click="normalizeWithConfirmedQuad">normalizeWithConfirmedQuad</button>
+      <button class="editor-border-btn" @click="confirmQuadForNormalization">Editor</button>
+      <button class="end-editor-btn" @click="normalizeWithConfirmedQuad">End Editor</button>
     </div>
     <div class="component-document-normalizer" ref="elRefs">
       <svg class="dce-bg-loading" viewBox="0 0 1792 1792"><path d="M1760 896q0 176-68.5 336t-184 275.5-275.5 184-336 68.5-336-68.5-275.5-184-184-275.5-68.5-336q0-213 97-398.5t265-305.5 374-151v228q-221 45-366.5 221t-145.5 406q0 130 51 248.5t136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5q0-230-145.5-406t-366.5-221v-228q206 31 374 151t265 305.5 97 398.5z"/></svg>
