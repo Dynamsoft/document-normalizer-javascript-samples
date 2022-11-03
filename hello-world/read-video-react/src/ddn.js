8ac695d796811ba5d17e9f34d898f21a96ad3140
SHEN <1014628363@qq.com>
2022-11-03 14:12:52 +0800

    DDNJS 1.0.10

--- hello-world/read-video-react/src/ddn.js
+++ hello-world/read-video-react/src/ddn.js
@@ -9,7 +9,7 @@ import { DocumentNormalizer } from 'dynamsoft-document-normalizer';
 /**
  * You can visit https://www.dynamsoft.com/customer/license/trialLicense?utm_source=github&product=ddn&package=js to get your own trial license good for 30 days.
  * Note that if you downloaded this sample from Dynamsoft while logged in, the above license key may already be your own 30-day trial license.
- * For more information, see https://www.dynamsoft.com/document-normalizer/programming/javascript/user-guide/?ver=1.0.0&utm_source=github#specify-the-license or contact support@dynamsoft.com.
+ * For more information, see https://www.dynamsoft.com/document-normalizer/programming/javascript/user-guide/?ver=1.0.10&utm_source=github#specify-the-license or contact support@dynamsoft.com.
  * LICENSE ALERT - THE END
  */
 
